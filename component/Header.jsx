f8477fa9c680a8b46a6505b424f9fa4562ee0a15
datistpham <giang10a1dz@gmail.com>
2023-05-05 20:59:22 +0700

    commit

--- component/Header.jsx
+++ component/Header.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import Cookies from "js-cookie"
+import { useRouter } from 'next/router'
 
 const Header = () => {
+  const router= useRouter()
+
   return (
     <div style={{width: "100%", height: 60, padding: "0 20px", display: "flex", justifyContent: "space-between", alignItems: "center"}}>
-      <div style={{fontSize: 48, fontWeight: 600, color: "#2e89ff"}}>Portal</div>
+      <div style={{fontSize: 48, fontWeight: 600, color: "#2e89ff"}} onClick={()=> router.push("/")}>Portal</div>
       <div onClick={()=> {
         Cookies.remove("uid")
         window.location.href= window.location.origin
